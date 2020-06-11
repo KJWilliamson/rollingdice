90501e439e4c869edf4a6783ccf78310a51d44e9
Kamela Williamson <kami63116@yahoo.com>
2020-06-11 11:17:15 -0700

    changed around js code

--- script.js
+++ script.js
@@ -10,26 +10,16 @@ function pairOfDice() {
     let dice2 = Math.floor( Math.random() * 6 ) +1;
     /* add dice together */
     let bothDice = dice1 + dice2;
-    
-    bothDice.innerHTML = bothDice;
 
-    //where do these need to be placed. Trace code!!
-    /* simulate 1000 rolls of a pair of dice */
+    //simulate 1000 rolls of a pair of dice 
     for(let i = 0; i <= 999; i++) {
-
-        
-    //the array that holds the numbers. how to get them in the array and in the right spot
-    let totalRolls = ["1: ", "2: ", "3: ", "4: ", "5: ", "6: "];
-
-
     
-    /* For each possible roll (2 through 12), count its frequency. You should use an array to keep track of these counts. */
-     let count = [0,0,0,0,0,0,0,0,0,0,0,0,0];
-     count[rollOfDice] = count[rollOfDice] + 1;
+        count[rollOfDice] = count[rollOfDice] + 1;
+        //For each possible roll (2 through 12), count its frequency. 
+        //You should use an array to keep track of these counts. 
+        let count = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+        }
+    }
 
-     //I need to take a break and then play with this. I'm confusing myself.
-    
-    }
-    }
     
     pairOfDice() 
