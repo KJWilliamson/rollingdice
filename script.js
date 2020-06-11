b4cf350e2643bf31747d4e12fe15c34f426dd596
Kamela Williamson <kami63116@yahoo.com>
2020-06-11 11:35:04 -0700

    changed js code

--- script.js
+++ script.js
@@ -3,16 +3,16 @@
 
 function pairOfDice() {
     
-    
+    for(let i = 0; i <= 999; i++) {
     /* for each die */
     /* two dice to roll get a number between 1-6 on each */
     let dice1 = Math.floor( Math.random() * 6 ) +1;
     let dice2 = Math.floor( Math.random() * 6 ) +1;
     /* add dice together */
-    let bothDice = dice1 + dice2;
+    let rollOfDice = dice1 + dice2;
 
     //simulate 1000 rolls of a pair of dice 
-    for(let i = 0; i <= 999; i++) {
+    
     
         count[rollOfDice] = count[rollOfDice] + 1;
         //For each possible roll (2 through 12), count its frequency. 
