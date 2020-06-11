618d46f2308ca4a8dfd4941a6957b31b55f24c1b
Kamela Williamson <kami63116@yahoo.com>
2020-06-11 10:07:19 -0700

    added code accidentally deleted

--- script.js
+++ script.js
@@ -24,8 +24,12 @@ function pairOfDice() {
         
     //the array that holds the numbers. how to get them in the array and in the right spot
     let totalRoles = ["1: ", "2: ", "3: ", "4: ", "5: ", "6: "];
+
+
+    
     /* For each possible roll (2 through 12), count its frequency. You should use an array to keep track of these counts. */
      let count = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+     count[rollOfDice] = count[rollOfDice] + 1;
 
      //I need to take a break and then play with this. I'm confusing myself.
     
