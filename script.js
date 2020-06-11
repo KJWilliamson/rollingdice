a4a014e4ac77ecd0058dbb5dd76c0456732405b8
Kamela Williamson <kami63116@yahoo.com>
2020-06-11 15:14:24 -0700

    fixed order of code and added more JS

--- script.js
+++ script.js
@@ -2,24 +2,22 @@
 
 
 function pairOfDice() {
-    
-    for(let i = 0; i <= 999; i++) {
-    /* for each die */
-    /* two dice to roll get a number between 1-6 on each */
+  
     let dice1 = Math.floor( Math.random() * 6 ) +1;
     let dice2 = Math.floor( Math.random() * 6 ) +1;
-    /* add dice together */
-    let rollOfDice = dice1 + dice2;
-
-    //simulate 1000 rolls of a pair of dice 
-    
-    
-        count[rollOfDice] = count[rollOfDice] + 1;
-        //For each possible roll (2 through 12), count its frequency. 
-        //You should use an array to keep track of these counts. 
-        let count = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+    let bothDice = dice1 + dice2;
+    return bothDice;
+  }
+    let count = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+  
+  
+    var outcome = [];
+  
+      //simulate 1000 rolls of a pair of dice 
+      for(let i = 0; i <= 999; i++) {
+        let rollOfDice = pairOfDice();
+          count[rollOfDice] = count[rollOfDice] + 1;
+          outcome.push(rollOfDice);
         }
-    }
-
-    
-    pairOfDice() 
+      
+        pairOfDice()
