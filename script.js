6e2929025fb07d2e8707cad12e6f42ad9834ab6c
Kamela Williamson <kami63116@yahoo.com>
2020-06-11 09:28:24 -0700

    added notes and js code

--- script.js
+++ script.js
@@ -3,16 +3,22 @@
 
 function pairOfDice() {
 
+    let dice1 = document.getElementById("dice1");
+    let dice2 = document.getElementById("dice2");
+    //need 
+
+    //where do these two below go
+    //the array that holds the numbers. how to get them in the array and in the right spot
     let bothDice = ["1: ", "2: ", "3: ", "4: ", "5: ", "6: "];
     /* For each possible roll (2 through 12), count its frequency. You should use an array to keep track of these counts. */
      let count = [0,0,0,0,0,0,0,0,0,0,0,0,0];
     
     /* for each die */
     /* two dice to roll get a number between 1-6 on each */
-    let die1 = Math.floor( Math.random() * 6 ) +1;
-    let die2 = Math.floor( Math.random() * 6 ) +1;
+    let dice1 = Math.floor( Math.random() * 6 ) +1;
+    let dice2 = Math.floor( Math.random() * 6 ) +1;
     /* add dice together */
-    let sum = die1 + die2;
+    let sum = dice1 + dice2;
     
     
     /* simulate 1000 rolls of a pair of dice */
