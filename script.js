87933b46153cad16dfea53d3390efff3d18c5643
Kamela Williamson <kami63116@yahoo.com>
2020-06-14 21:16:59 -0700

    changed graph to orange

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function pairOfDice() {
             const bar = document.createElement("div");
             bar.innerText = key;
             bar.style.color = "white";
-            bar.style.backgroundColor = "grey";
+            bar.style.backgroundColor = "orange";
             bar.style.width = graph[key] + "px";
             bar.style.marginTop = "10px";
             bar.style.marginBottom = "10px";
@@ -36,36 +36,8 @@ function pairOfDice() {
         }
          
           return barGraph;
-         
-      };
-           
+      }
+    
             pairOfDice()
             document.write(count);
-            
             document.body.appendChild(createBarGraph(count));
-
-            
-          //figure out how to use code from mini lesson to also get graph to work!!
-          
-        /* 
-      for(i=1; i<=3; i++) {
-        // Create a div, with class "bar", and set the width to 100px.
-        var newElement = document.createElement("div");
-        newElement.className = "bar";
-        newElement.style.width = i*100 + "px";
-
-        // Place a text label inside the new div.
-        var newText = document.createTextNode("#: #" + i);
-        newElement.appendChild(newText);
-
-        // Put the new div on the page inside the existing element "d1".
-        var destination = document.getElementById("d1");
-        destination.appendChild(newElement);
-      }
-      */
-           
-
-      
-      
-
-      
