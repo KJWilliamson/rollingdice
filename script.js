8414e38b3bfb4437da3bd5bae4b884687c18049f
Kamela Williamson <kami63116@yahoo.com>
2020-06-14 21:00:52 -0700

    added graph and text

--- script.js
+++ script.js
@@ -19,5 +19,53 @@ function pairOfDice() {
 
       }
       
-        pairOfDice()
-        document.write(count);
+      function createBarGraph(graph) {
+        
+          const barGraph = document.createElement("div");
+            for (let key in graph) {
+            const bar = document.createElement("div");
+            bar.innerText = key;
+            bar.style.color = "white";
+            bar.style.backgroundColor = "grey";
+            bar.style.width = graph[key] + "px";
+            bar.style.marginTop = "10px";
+            bar.style.marginBottom = "10px";
+            barGraph.appendChild(bar);
+    
+           
+        }
+         
+          return barGraph;
+         
+      };
+           
+            pairOfDice()
+            document.write(count);
+            
+            document.body.appendChild(createBarGraph(count));
+
+            
+          //figure out how to use code from mini lesson to also get graph to work!!
+          
+        /* 
+      for(i=1; i<=3; i++) {
+        // Create a div, with class "bar", and set the width to 100px.
+        var newElement = document.createElement("div");
+        newElement.className = "bar";
+        newElement.style.width = i*100 + "px";
+
+        // Place a text label inside the new div.
+        var newText = document.createTextNode("#: #" + i);
+        newElement.appendChild(newText);
+
+        // Put the new div on the page inside the existing element "d1".
+        var destination = document.getElementById("d1");
+        destination.appendChild(newElement);
+      }
+      */
+           
+
+      
+      
+
+      
